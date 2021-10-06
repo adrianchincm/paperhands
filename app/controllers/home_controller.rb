d1f56795b4f75c8e57a374358df05a2ad2f9007b
Adrian Chin <syncing95@gmail.com>
2021-10-06 21:07:23 +0800

    add httparty gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
+require "httparty"
+
 class HomeController < ApplicationController
   def index
+
+
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    response = HTTParty.get(url)    
+    price = response.parsed_response.to_a
+    p price[0]["id"]
   end
 end
