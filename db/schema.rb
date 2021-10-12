2c2a1b5455e6bb0040f6359d00c837a5041e02c4
Adrian Chin <syncing95@gmail.com>
2021-10-12 21:35:38 +0800

    finished sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_10_11_141239) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
