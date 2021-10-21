04128f5fef750c7a4db45a4a7bb937d19d77d7b9
Adrian Chin <syncing95@gmail.com>
2021-10-21 22:42:52 +0800

    add before_save callback to initialise cash balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save :add_cash_balance
   has_many :orders, dependent: :destroy
+  has_many :portfolios, dependent: :destroy  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
@@ -7,6 +10,15 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable
+
+  
+
+  private
+
+  def add_cash_balance
+    self.cash_balance = 1000000.00
+  end
+
 end
 
 
